e388314d92b59b29b121491b60aff3e5b594972e
blackdesert-itsokay <jhaowei.alpha@gmail.com>
2017-06-23 13:11:15 +0800

    fixed parsing error

--- commands/delivery.js
+++ commands/delivery.js
@@ -5,11 +5,15 @@ let delivery = {
   help: '告訴你當前時段的皇室納貢/皇室釣魚的更新分流',
   func: (bot, message, args) => {
     let time = imperialTimer.delivery.time;
-    let text = imperialTimer.delivery.channel;
+    let info = imperialTimer.delivery.info;
+    let text = '';
     try {
-      let arr = text.replace(/<li>/g, "").split(/<\/li>/g);
-      message.channel.send(`皇室納貢/皇室釣魚的更新\n${time}\n${arr}`);
-    } catch(err) {
+      for (let i = 0; i < info.length; i++) {
+        text += info[i].innerHTML;
+        if (i != info.length - 1) text += ', ';
+      }
+      message.channel.send(`皇室納貢/皇室釣魚的更新\n${time}\n${text}`);
+    } catch (err) {
       message.channel.send(`皇室納貢/皇室釣魚的更新\n${time}\n目前沒有更新唷!!`);
     }
   }
